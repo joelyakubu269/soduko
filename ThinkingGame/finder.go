ebaf81d6b1d730e75ad745987cdd8296611a340b
joyakubu <joelyakubu269@gmail.com>
2026-06-02 19:17:56 +0100

    refractoring my code

--- ThinkingGame/finder.go
+++ ThinkingGame/finder.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
+func findEmptySpace(s []string) (int, int, bool) {
 
-func findEmptySpace(s []string) ([]string, error) {
-	buffer := []string{}
 	for i := 0; i < 9; i++ { // have a grid  of nine rows
 		for j := 0; j < 9; j++ {
 			if s[i][j] == '.' { // check if the values of each row per column if its empty
-				buffer = append(buffer, fmt.Sprintf("%d,%d", i, j)) // append the position in a slice of strings
+				return i, j, true
 			}
 		}
 	}
-	return buffer, nil
+	return 0, 0, false
 }
