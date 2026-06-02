b520a3d0846b37abee9144677f9b2640cb8c44d8
joyakubu <joelyakubu269@gmail.com>
2026-06-02 03:34:12 +0100

    creating a function to find empty space

---
+++ ThinkingGame/finder.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func findEmptySpace(s []string) []string {
+	buffer := []string{}
+	for i := 0; i < 9; i++ { // have a grid  of nine rows
+		for j := 0; j < 9; j++ {
+			if s[i][j] == '.' { // check if the values of each row per column if its empty
+				buffer = append(buffer, fmt.Sprintf("%d,%d", i, j)) // append the position in a slice of strings
+			}
+		}
+
+	}
+}
