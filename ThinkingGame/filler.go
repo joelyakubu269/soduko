ffd70f29b869f5424ef8eb441c3805cdcde97507
joyakubu <joelyakubu269@gmail.com>
2026-06-02 05:37:35 +0100

    writng a function to validate the values i will fill according to the rules

--- ThinkingGame/filler.go
+++ ThinkingGame/filler.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 func FillEmptySpace(s []string) *[]string {
+	rows, cols, err := extractPositions(s)
+	if err == nil {
+		for i := 0; i < len(rows); i++ {
+			iVal := rows[i]
+			jVal := cols[i]
+			row := []rune(s[iVal])
+			for j := 0; j < 9; j++ {
+				row[jVal] = rune('0' + j)
+				s[iVal] = string(row)
+				valid := validateDigits(s)
+				if !valid {
+
+				}
+			}
+		}
+	}
 
 }
 func extractPositions(s []string) ([]int, []int, error) {
 	rows := []int{}
 	cols := []int{}
-	for i, r := range s {
-		parts := strings.Split(string(r[i]), ",")
+	for _, r := range s {
+		parts := strings.Split(r, ",")
+		if len(parts) != 2 {
+			return nil, nil, fmt.Errorf("invalid format")
+		}
 
 		firstNum, err := strconv.Atoi(parts[0])
 		if err != nil {
@@ -20,7 +40,7 @@ func extractPositions(s []string) ([]int, []int, error) {
 		}
 		rows = append(rows, firstNum)
 
-		secondNum, err := strconv.Atoi(parts[0])
+		secondNum, err := strconv.Atoi(parts[1])
 		if err != nil {
 			return nil, nil, err
 		}
--- ThinkingGame/validateDigit.go
+++ ThinkingGame/validateDigit.go
@@ -4,12 +4,13 @@ func validateDigits(s []string) bool {
 	valid := true
 	for i := 0; i < 9; i++ { // checking the rows
 		for j := 0; j < 9; j++ {
-			for k := 0; k < 3; k++ {
-				if s[i][j] == s[i][k] {
+			for k := 0; k < 9; k++ {
+				if s[i][j] == s[j][k] {
 					valid = false
 					return valid
 				}
 			}
 		}
 	}
+	return valid
 }
